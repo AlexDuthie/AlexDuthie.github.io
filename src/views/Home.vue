6e699dcbd8a2686fe0e8bce20d6d809d6936b2d9
Alex Duthie <alexduthiemain@outlook.com>
2021-02-25 22:48:18 +0000

    About view removed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Alex Duthie's Portfolio (Coming Soon)"/>
   </div>
 </template>
 
